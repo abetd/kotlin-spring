9cdfc7935b74c7b69df98fb7464eee5ff08b06e9
abetd <tadanori.abe@gmail.com>
2018-11-24 00:15:28 +0900

    coreプロジェクトを追加

---
+++ core/src/test/kotlin/com/example/core/SandboxTest.kt
@@ -0,0 +1,12 @@
+package com.example.core
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class SandboxTest {
+    @Test
+    fun testHello() {
+        val actual = "Hello World."
+        assertEquals(actual, "Hello World.")
+    }
+}
