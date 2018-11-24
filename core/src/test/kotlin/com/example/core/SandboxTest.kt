c02130967af0290a47172910913dcc084ad6e227
abetd <tadanori.abe@gmail.com>
2018-11-24 17:19:41 +0900

    junit5に変更

--- core/src/test/kotlin/com/example/core/SandboxTest.kt
+++ core/src/test/kotlin/com/example/core/SandboxTest.kt
@@ -1,11 +1,15 @@
 package com.example.core
 
-import org.junit.Test
-import org.junit.Assert.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
 
 class SandboxTest {
+
     @Test
-    fun testHello() {
+    @DisplayName("Hello World")
+    fun hello() {
         val actual = "Hello World."
         assertEquals(actual, "Hello World.")
     }
