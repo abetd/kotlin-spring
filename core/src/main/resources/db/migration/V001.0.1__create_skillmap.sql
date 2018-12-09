370c28234caed07db1d9ac0e683090340d5f3a92
abetd <tadanori.abe@gmail.com>
2018-12-09 23:49:09 +0900

    スキルマップのテーブルを追加

---
+++ core/src/main/resources/db/migration/V001.0.1__create_skillmap.sql
@@ -0,0 +1,33 @@
+CREATE TABLE members (
+    id int NOT NULL PRIMARY KEY,
+    name varchar(32) NOT NULL
+);
+
+CREATE TABLE skills (
+    id int NOT NULL PRIMARY KEY,
+    name varchar(32) NOT NULL
+);
+
+CREATE TABLE tags (
+    id int NOT NULL PRIMARY KEY,
+    name varchar(32) NOT NULL
+);
+
+CREATE TABLE skill_tag_relation (
+    tag_id int NOT NULL REFERENCES tags(id),
+    skill_id int NOT NULL REFERENCES skills(id),
+    CONSTRAINT skill_tag_relation_pkey PRIMARY KEY(tag_id,skill_id)
+);
+
+CREATE TABLE levels (
+    id int NOT NULL PRIMARY KEY,
+    mark CHAR NOT NULL,
+    discription varchar(32) NOT NULL
+);
+
+CREATE TABLE skill_maps (
+    member_id int NOT NULL REFERENCES members(id),
+    skill_id int NOT NULL REFERENCES skills(id),
+    level_id int NOT NULL REFERENCES levels(id),
+    CONSTRAINT skill_maps_pkey PRIMARY KEY(member_id, skill_id, level_id)
+);
